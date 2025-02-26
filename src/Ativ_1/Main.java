c9aa7ab618bf2d622362d297e948d33ac33718c4
Thallys_San <ricardo.viasolo142@gmail.com>
2025-02-25 22:10:55 -0300

    Update Main.java

--- src/Ativ_1/Main.java
+++ src/Ativ_1/Main.java
@@ -2,13 +2,18 @@ package Ativ_1;
 
 public class Main {
     public static void main(String[] args) {
-        Ingresso ingresso1 = new Ingresso();
 
-        ingresso1.setValor(10);
+        Ingresso ingresso1 = new Ingresso(10);
         ingresso1.imprimeValor();
 
-        Ingresso ingresso2 = new Normal();
-        ingresso2.setValor(15);
-        ingresso2.imprimeValor();
+        Normal ingressoNormal = new Normal(15);
+        ingressoNormal.imprimeValor();
+
+        Vip ingressoVip = new Vip(15, 10);
+        ingressoVip.setIngressoVip(20);
+        ingressoVip.imprimeValor();
     }
 }
+
+
+
